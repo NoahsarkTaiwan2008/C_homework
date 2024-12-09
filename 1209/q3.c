12c6841bf13e291efe0161bc2fa025b18094d8a1
hikari <ben109092ben@gmail.com>
2024-12-09 15:41:43 +0800

    update

--- 1209/q3.c
+++ 1209/q3.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
 
 int main() {
-    int row;
-    int col;
-    printf("輸入矩陣A的行: ");
+    int row, col;
+    printf("輸入矩陣A的行數: ");
     scanf("%d", &row);
-    printf("輸入矩陣A的列: ");
+    printf("輸入矩陣A的列數: ");
     scanf("%d", &col);
+    
     int a[row][col];
+    int b[col][row];
 
-    for (int i = 0;)
-    printf("輸入")
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            printf("輸入a array的第%d列與第%d行數值 [%d][%d]: ", i+1, j+1, i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            b[j][i] = a[i][j];
+        }
+    }
+
+    printf("矩陣A的內容是:\n");
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            printf("A[%d][%d]= %d\t", i, j, a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("矩陣B（A的轉置）是:\n");
+    for (int i = 0; i < col; i++) {
+        for (int j = 0; j < row; j++) {
+            printf("B[%d][%d]= %d\t", i, j, b[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
